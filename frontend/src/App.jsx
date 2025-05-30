8a06624894726382f46e0b1e169aa080aeff3cbe
Michele del Giudice <dgmichele96@gmail.com>
2025-05-30 18:59:13 +0200

    test unione client-server

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,30 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [apiMessage, setApiMessage] = useState('');
+
+  useEffect(() => {
+    const fetchMessage = async () => {
+      try {
+        const res = await axios.get(`${import.meta.env.VITE_API_BASE_URL}/`);
+        setApiMessage(res.data); // dovrebbe essere "API pronta per partire!"
+      } catch (err) {
+        console.error('Errore nella chiamata API:', err);
+        setApiMessage('Errore nel collegamento al server!');
+      }
+    };
+
+    fetchMessage();
+  }, []);
 
   return (
-    <>
-    <h1>Benvenuto!</h1>
-    </>
-  )
+    <div>
+      <h1>Benvenuto!</h1>
+      <p>{apiMessage}</p>
+    </div>
+  );
 }
 
-export default App
+export default App;
+
