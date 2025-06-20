bcd19286987650ba3863c5e6a5ed125a5581bed4
Michele del Giudice <dgmichele96@gmail.com>
2025-06-20 16:11:25 +0200

    fix stile

--- frontend/src/pages/NotFoundPage.jsx
+++ frontend/src/pages/NotFoundPage.jsx
@@ -1,17 +1,20 @@
 import { Link } from 'react-router-dom';
 import styles from '../asset/css/NotFoundPage.module.css';
 import logo from '../asset/images/Logo.webp'
+import { FaArrowRight } from "react-icons/fa";
 
 export default function NotFoundPage() {
   return (
     <>
     <header className={styles.header}>
-      <Link to="/"><img className={styles.logo} src={logo} alt="logo" /></Link>
+      <div className={styles.headerContainer}>
+        <Link to="/"><img className={styles.logo} src={logo} alt="logo" /></Link>
+      </div>
     </header>
     <div className={styles.container}>
       <h1>404</h1>
       <p>Oops! Pagina non trovata.</p>
-      <Link to="/">Torna alla Home</Link>
+      <Link to="/">Torna alla Home <FaArrowRight className={styles.arrowIcon} /></Link>
     </div>
     <footer>
         <p>© {new Date().getFullYear()} Bich Immobiliare. Tutti i diritti riservati.</p>
